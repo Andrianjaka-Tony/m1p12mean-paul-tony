c12e255ba95e3e4a27a980ed51bb699199994466
Tony <andrianjaka.tony@gmail.com>
2025-03-05 15:31:39 +0300

    Hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,6 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `
-    <div class="h-screen w-screen flex items-center justify-center">
-      <h1 class="text-8xl tracking-tighter">Vroom Vroom🥳</h1>
-    </div>
-
-    <router-outlet />
-  `,
-  styles: [],
+  template: `<router-outlet />`,
 })
 export class AppComponent {}
