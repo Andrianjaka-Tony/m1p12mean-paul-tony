a4965993d7dc2cc85ebd3652b17dfabd58a31db7
Tony <andrianjaka.tony@gmail.com>
2025-03-06 12:49:54 +0300

    Footer section

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-section',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styles: ``,
+})
+export class FooterComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { NavigationComponent } from '../../components/navigation/navigation.comp
 import { ServicesComponent } from '../../components/services/services.component';
 import { ProcessComponent } from '../../components/process/process.component';
 import { SubscriptionComponent } from '../../components/subscription/subscription.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +16,7 @@ import { SubscriptionComponent } from '../../components/subscription/subscriptio
     ServicesComponent,
     ProcessComponent,
     SubscriptionComponent,
+    FooterComponent,
   ],
   templateUrl: './home.component.html',
 })
