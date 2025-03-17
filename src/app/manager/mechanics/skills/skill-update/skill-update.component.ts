0266d82297673b403ba5bb816e1b33f998fcebf2
Tony <andrianjaka.tony@gmail.com>
2025-03-17 15:40:43 +0300

    Skills update form with controlled input

--- src/app/manager/mechanics/skills/skill-update/skill-update.component.ts
+++ src/app/manager/mechanics/skills/skill-update/skill-update.component.ts
@@ -1,26 +1,16 @@
-import {
-  Component,
-  inject,
-  input,
-  OnInit,
-  output,
-  signal,
-} from '@angular/core';
+import { Component, inject, input, OnInit, output } from '@angular/core';
 import { ModalComponent } from '../../../../components/modal/modal.component';
 import { ButtonComponent } from '../../../../components/button/button.component';
 import { Award, LucideAngularModule, X } from 'lucide-angular';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgClass } from '@angular/common';
 import { SkillService } from '../../../../services/mechanic/skill.service';
 import { Skill } from '../../../../models/mechanic/skill.model';
 import { catchError, finalize } from 'rxjs';
 import { Response } from '../../../../models/response.model';
 import { toast } from '../../../../components/toast/toast.component';
+import { createForm } from 'src/app/utils/create-form';
+import { ControlledInputComponent } from '../../../../components/controlled-input/controlled-input.component';
 
 @Component({
   selector: 'skill-update',
@@ -30,6 +20,7 @@ import { toast } from '../../../../components/toast/toast.component';
     LucideAngularModule,
     ReactiveFormsModule,
     NgClass,
+    ControlledInputComponent,
   ],
   templateUrl: './skill-update.component.html',
   styles: ``,
@@ -45,34 +36,43 @@ export class SkillUpdateComponent implements OnInit {
   readonly close = output();
   readonly afterSubmit = output();
 
-  readonly isSubmitted = signal<boolean>(false);
-  readonly isSending = signal<boolean>(false);
-  readonly message = signal<string>('');
-  readonly form = new FormGroup({
-    label: new FormControl('', [Validators.required]),
-  });
+  readonly fields = [
+    { name: 'label', defaultValue: '', validators: [Validators.required] },
+  ];
+  readonly form = createForm(this.fields);
+  readonly fieldsControls = [
+    {
+      id: 'label',
+      label: 'Intitulé',
+      type: 'text',
+      controleName: 'label',
+      isSubmitted: this.form.isSubmitted,
+      form: this.form.formGroup,
+      messages: [{ message: "L'intitulé est requis.", validator: 'required' }],
+    },
+  ];
 
   ngOnInit(): void {
-    this.form.setValue({ label: this.skill().label });
+    this.form.formGroup.setValue({ label: this.skill().label });
   }
 
   async handleSubmit() {
-    this.isSubmitted.set(true);
-    if (this.form.valid) {
-      this.isSending.set(true);
+    this.form.isSubmitted.set(true);
+    if (this.form.formGroup.valid) {
+      this.form.isSending.set(true);
 
-      const skill = this.form.value as Skill;
+      const skill = this.form.formGroup.value as Skill;
       skill._id = this.skill()._id;
       this.skillService
         .update(skill as Skill)
         .pipe(
           catchError((e) => {
             const error = e.error as Response<undefined>;
-            this.message.set(error.message);
+            this.form.message.set(error.message);
             throw e;
           }),
           finalize(() => {
-            this.isSending.set(false);
+            this.form.isSending.set(false);
           })
         )
         .subscribe(() => {
