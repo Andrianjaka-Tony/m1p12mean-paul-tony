dd33f531dfef8531baaa489239034f1e9d18eb90
Tony <andrianjaka.tony@gmail.com>
2025-03-18 19:46:05 +0300

    Adding animation for modal component

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,11 +1,38 @@
-import { Component, output } from '@angular/core';
+import { Component, OnInit, output } from '@angular/core';
+import gsap from 'gsap';
 
 @Component({
   selector: 'modal',
   imports: [],
   templateUrl: './modal.component.html',
-  styles: ``,
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
   readonly close = output();
+
+  ngOnInit(): void {
+    this.animateIn();
+  }
+
+  animateIn() {
+    gsap.fromTo(
+      '.modal-overlay, .modal-content',
+      { opacity: 0 },
+      { opacity: 1, duration: 0.1, ease: 'power1.inOut' }
+    );
+  }
+
+  handleClose() {
+    gsap.fromTo(
+      '.modal-overlay, .modal-content',
+      { opacity: 1 },
+      {
+        opacity: 0,
+        duration: 0.1,
+        ease: 'power1.inOut',
+        onComplete: () => {
+          this.close.emit();
+        },
+      }
+    );
+  }
 }
