29f1f68ca658f2609d8434a3dcf92003e2215bf5
Tony <andrianjaka.tony@gmail.com>
2025-03-19 22:13:44 +0300

    SVG progression for quote card

--- src/app/components/quote-card/quote-card.component.ts
+++ src/app/components/quote-card/quote-card.component.ts
@@ -5,6 +5,13 @@ import { BadgeComponent } from '../badge/badge.component';
   selector: 'quote-card',
   imports: [BadgeComponent],
   templateUrl: './quote-card.component.html',
-  styles: ``,
+  styles: `
+    .quote-card-svg {
+      stroke-width: 4px;
+      stroke-dasharray: calc(2 * 3.14 * 22);
+      stroke-dashoffset: calc(2 * 3.14 * 22 * (1 - 0.84));
+      transform: rotate(-90deg);
+    }
+  `,
 })
 export class QuoteCardComponent {}
