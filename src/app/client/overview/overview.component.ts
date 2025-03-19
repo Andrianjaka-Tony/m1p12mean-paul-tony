7cfadd4fc6a9671ea6e95d73de1fe484b67fac77
Tony <andrianjaka.tony@gmail.com>
2025-03-19 21:03:16 +0300

    Quote card component

--- src/app/client/overview/overview.component.ts
+++ src/app/client/overview/overview.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CarListComponent } from './car-list/car-list.component';
+import { QuoteCardComponent } from '../../components/quote-card/quote-card.component';
 
 @Component({
   selector: 'overview-page',
-  imports: [CarListComponent],
+  imports: [CarListComponent, QuoteCardComponent],
   templateUrl: './overview.component.html',
   styles: ``,
 })
---
+++ src/app/components/quote-card/quote-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { BadgeComponent } from '../badge/badge.component';
+
+@Component({
+  selector: 'quote-card',
+  imports: [BadgeComponent],
+  templateUrl: './quote-card.component.html',
+  styles: ``,
+})
+export class QuoteCardComponent {}
