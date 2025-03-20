aff1fdb7b997fc4c274fd935683c0b141ef2730f
Tony <andrianjaka.tony@gmail.com>
2025-03-20 16:47:53 +0300

    Fetchin client cars

--- src/app/models/clients/car.model.ts
+++ src/app/models/clients/car.model.ts
@@ -1,7 +1,21 @@
+import { User } from '../auth/user.model';
+import { Brand } from './brand.model';
+import { CarType } from './car-type.model';
+
 export type CarFromForm = {
   id_type_vehicle: string;
   id_brand_vehicle: string;
   id_client: string;
+  registration_number: string;
+  year: number;
+  model: string;
+};
+
+export type CarFromClient = {
+  _id: string;
+  id_type_vehicle: CarType;
+  id_brand_vehicle: Brand;
+  id_client: User & { _id: string };
   registration_number: string;
   year: number;
   model: string;
--- src/app/services/clients/car.service.ts
+++ src/app/services/clients/car.service.ts
@@ -1,20 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { User } from 'src/app/models/auth/user.model';
-import { CarFromForm } from 'src/app/models/clients/car.model';
+import { CarFromClient, CarFromForm } from 'src/app/models/clients/car.model';
 import { Response } from 'src/app/models/response.model';
 import { createHeaders } from 'src/app/utils/headers';
-import { userDataStoreName } from 'src/app/utils/sotre';
+import { userDataStoreName, userTokenStoreName } from 'src/app/utils/sotre';
 import { apiUrl } from 'src/app/utils/url';
+import { SignService } from '../auth/sign.service';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CarService {
+  readonly signService = inject(SignService);
   readonly http = inject(HttpClient);
 
   save(car: CarFromForm) {
     const headers = createHeaders();
+
     const client = JSON.parse(
       localStorage.getItem(userDataStoreName) || '{}'
     ) as { _id: string } & User;
@@ -23,6 +27,20 @@ export class CarService {
     return this.http.post<Response<undefined>>(
       `${apiUrl}/api/client_vehicle`,
       car,
+      { headers }
+    );
+  }
+
+  findCarsByClient() {
+    const headers = createHeaders();
+    const token = localStorage.getItem(userTokenStoreName);
+    if (!token) {
+      this.signService.signOut();
+      return of(undefined);
+    }
+
+    return this.http.get<Response<CarFromClient[]>>(
+      `${apiUrl}/api/client_vehicle/user/${token}`,
       { headers }
     );
   }
