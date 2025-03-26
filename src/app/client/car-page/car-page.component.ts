6481e70a37b35a8a1858b1b1f6ef4bfc58dbe4b1
Tony <andrianjaka.tony@gmail.com>
2025-03-26 11:18:15 +0300

    Car page header with link to New Quote Page

--- src/app/client/car-page/car-page.component.ts
+++ src/app/client/car-page/car-page.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Grid, List, LucideAngularModule, Search } from 'lucide-angular';
+import { ButtonComponent } from 'src/app/components/button/button.component';
 
 @Component({
   selector: 'car-page',
-  imports: [],
+  imports: [LucideAngularModule, ButtonComponent, RouterLink],
   templateUrl: './car-page.component.html',
   styles: ``,
 })
-export class CarPage {}
+export class CarPage implements OnInit {
+  readonly search = Search;
+  readonly grid = Grid;
+  readonly list = List;
+
+  readonly route = inject(ActivatedRoute);
+  id: string = '';
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const id = params.get('id');
+      if (id != null) {
+        this.id = id;
+      }
+    });
+  }
+}
