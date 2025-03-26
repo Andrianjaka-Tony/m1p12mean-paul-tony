5f34014b89e059ee1733f6ac75bd5096f094e60d
Tony <andrianjaka.tony@gmail.com>
2025-03-26 11:26:40 +0300

    Adding services attributes for new-quote page

--- src/app/client/new-quote/new-quote.component.ts
+++ src/app/client/new-quote/new-quote.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, OnInit, signal } from '@angular/core';
+import { catchError, finalize } from 'rxjs';
+import { Service } from 'src/app/models/mechanic/services.model';
+import { Pageable } from 'src/app/models/pageable.model';
+import { ServicesService } from 'src/app/services/mechanic/services.service';
 
 @Component({
   selector: 'new-quote',
@@ -6,4 +10,33 @@ import { Component } from '@angular/core';
   templateUrl: './new-quote.component.html',
   styles: ``,
 })
-export class NewQuotePage {}
+export class NewQuotePage implements OnInit {
+  readonly servicesService = inject(ServicesService);
+
+  readonly isLoading = signal<boolean>(true);
+  readonly page = signal<number>(1);
+  readonly services = signal<Service[]>([] as Service[]);
+  readonly pageable = signal<Pageable>({} as Pageable);
+
+  ngOnInit(): void {
+    this.findServices();
+  }
+
+  findServices() {
+    this.servicesService
+      .findServices(this.page() || 1)
+      .pipe(
+        catchError((e) => {
+          throw e;
+        }),
+        finalize(() => {
+          this.isLoading.set(false);
+        })
+      )
+      .subscribe((response) => {
+        console.log(response.data.services);
+        this.services.set(response.data.services);
+        this.pageable.set(response.data as Pageable);
+      });
+  }
+}
