d4cdac259475df67f670e3ca289a02e8668ec062
Tony <andrianjaka.tony@gmail.com>
2025-04-04 12:23:35 +0300

    Push

--- src/app/client/car-page/quote-card/quote-card.component.ts
+++ src/app/client/car-page/quote-card/quote-card.component.ts
@@ -1,4 +1,10 @@
-import { Component, input, output, WritableSignal } from '@angular/core';
+import {
+  Component,
+  input,
+  OnInit,
+  output,
+  WritableSignal,
+} from '@angular/core';
 import { QuoteFromFind } from 'src/app/models/clients/quote.model';
 import { FormatDatePipe } from 'src/app/pipes/date-format.pipe';
 import { NumberFormatPipe } from 'src/app/pipes/number-format.pipe';
@@ -13,7 +19,7 @@ import { NgClass } from '@angular/common';
     .quote-card-svg {
       stroke-width: 4px;
       stroke-dasharray: calc(2 * 3.14 * 22);
-      stroke-dashoffset: calc(2 * 3.14 * 22 * (1 - 0.84));
+      stroke-dashoffset: calc(2 * 3.14 * 22 * (1 - 0.45));
       transform: rotate(-90deg);
     }
   `,
